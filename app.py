d1c614e5fdc6b3cbda9550d8487385d70329718a
Quei-An Chen <kwea123@gmail.com>
2017-10-05 22:43:01 +0800

    fb

--- app.py
+++ app.py
@@ -21,20 +21,27 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
-    # endpoint for processing incoming messaging events
     data = request.get_json()
- 
     if data["object"] == "page":
- 
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
- 
                 if messaging_event.get("message"):  # someone sent us a message
- 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    send_message(sender_id, "roger that!")
+                    if "beauty" in message_text:
+                        text = ""
+                        r = requests.get("https://www.ptt.cc/bbs/beauty/index.html")
+                        soup = bs(r.text,"lxml")
+                        page = soup.find("div", class_="btn-group-paging")
+                        prev = page.find_all("a")[1]['href'] # previous page
+                        r = requests.get("https://www.ptt.cc"+prev)
+                        soup = bs(r.text,"lxml")
+                        for div in soup.find_all("div", class_="r-ent"):
+                            a = div.find_all("a")
+                            if len(a)>0:
+                                text += "https://www.ptt.cc"+a[0]['href']+'\n'
+                        send_message(sender_id, text)
  
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
